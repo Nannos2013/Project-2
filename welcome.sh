6b02dbd3c6e3fd51657eaf6a044def5eced7312a
Nannos2013 <71235343+Nannos2013@users.noreply.github.com>
2020-10-07 17:30:31 -0500

    Update welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -1,8 +1,5 @@
-nannos2013@kali:~$ a=4
-nannos2013@kali:~$ b=8
-nannos2013@kali:~$ echo $a
-4
-nannos2013@kali:~$ echo $b
-8
-nannos2013@kali:~$ echo $[$a + $b]
-12
+nannos2013@kali:~$ vi welcome.sh
+nannos2013@kali:~$ chmod +x welcome.sh
+nannos2013@kali:~$ ./welcome.sh
+greeting back nannos2013! Today is Wednesday, which is the best day of the entire week!
+Your Bash shell version is: . Enjoy!
