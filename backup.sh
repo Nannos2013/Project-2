c89d9a64ad77abf27b5e40ecba8067ff68d39db6
Nannos2013 <71235343+Nannos2013@users.noreply.github.com>
2020-10-07 18:05:51 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,8 @@
 nannos2013@kali:~$ vi backup.sh
 nannos2013@kali:~$ chmod +x backup.sh
 nannos2013@kali:~$ ./backup.sh
-Backup of /home/nannos2013 completed! Details about the output backup file:
--rw-r--r-- 1 nannos2013 nannos2013 1224872 Oct  7 17:43 /tmp/nannos2013_home_2020-10-07_174345.tar.gz
+FIles to be included:70
+Directories to be included:69
+Backup of /home/nannos2013 completed!
+Details about the output backup file:
+-rw-r--r-- 1 nannos2013 nannos2013 1224425 Oct  7 18:05 /tmp/nannos2013_home_2020-10-07_180501.tar.gz
