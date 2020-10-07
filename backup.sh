e239646d6d92dc41fd27c1ba62ddc5db75d6b133
Nannos2013 <71235343+Nannos2013@users.noreply.github.com>
2020-10-07 17:44:19 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,5 @@
 nannos2013@kali:~$ vi backup.sh
 nannos2013@kali:~$ chmod +x backup.sh
 nannos2013@kali:~$ ./backup.sh
-tar: Removing leading `/' from member names
 Backup of /home/nannos2013 completed! Details about the output backup file:
--rw-r--r-- 1 nannos2013 nannos2013 1228461 Oct  7 17:40 /tmp/nannos2013_home_2020-10-07_174009.tar.gz
+-rw-r--r-- 1 nannos2013 nannos2013 1224872 Oct  7 17:43 /tmp/nannos2013_home_2020-10-07_174345.tar.gz
