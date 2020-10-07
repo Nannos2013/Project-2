00c05b8792a4532ed2ca338fdb52f4564fe5b472
Nannos2013 <71235343+Nannos2013@users.noreply.github.com>
2020-10-07 18:31:39 -0500

    Create if_else.sh

---
+++ if_else.sh
@@ -0,0 +1,5 @@
+nannos2013@kali:~$ vi if_else.sh
+nannos2013@kali:~$ chmod +x if_else.sh
+nannos2013@kali:~$ ./if_else.sh
+./if_else.sh: line 6: [: 400: unary operator expected
+400 os greater than 200!
