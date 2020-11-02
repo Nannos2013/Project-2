3c48c2b4fa2503a56aee7317c202a978adea24f1
gespho <71243256+gespho@users.noreply.github.com>
2020-11-01 19:49:12 -0600

    Update if_else.sh

--- if_else.sh
+++ if_else.sh
@@ -1,5 +1,19 @@
-nannos2013@kali:~$ vi if_else.sh
-nannos2013@kali:~$ chmod +x if_else.sh
-nannos2013@kali:~$ ./if_else.sh
-./if_else.sh: line 6: [: 400: unary operator expected
-400 os greater than 200!
+#If-else demonstration code
+
+num_a=400
+num_b=200
+
+if [ $num_a -lt $num_b ]; then
+	echo "$num_a ls less than $num_b!"
+else
+	echo "$num_a is greather than $num_b!"
+fi
+
+#completely wrong, this is from the command line not actual code
+#The calls are right but no actual code was present
+#what is this
+#nannos2013@kali:~$ vi if_else.sh
+#nannos2013@kali:~$ chmod +x if_else.sh
+#nannos2013@kali:~$ ./if_else.sh
+#./if_else.sh: line 6: [: 400: unary operator expected
+#400 os greater than 200!
